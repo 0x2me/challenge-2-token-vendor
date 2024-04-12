07ecf0bb4ded3925d05ecfd3daaf986a59ed065e
Your Name <philip.tuomey@tribal.credit>
2024-04-12 21:45:08 +0100

    Update default network to "sepolia" in hardhat.config.ts Update _mint function in YourToken.sol Update targetNetworks to [chains.sepolia] in scaffold.config.ts Update Vendor contract with buyTokens, withdraw, and sellTokens functions Update deployVendor function in 01_deploy_vendor.ts

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,21 +1,47 @@
 pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract Vendor {
-  // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
-  YourToken public yourToken;
+contract Vendor is Ownable {
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event SellTokens(address seller, uint256 amountOfTokens, uint256 amountOfETH);
 
+  using SafeERC20 for IERC20;
+
+  IERC20 public yourToken;
+  uint public tokensPerEth = 100; 
   constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
+    yourToken = IERC20(tokenAddress);
   }
 
   // ToDo: create a payable buyTokens() function:
+  function buyTokens() payable public {
+    require(msg.value > 0, "You need to send some Ether");
+    uint256 amountOfTokens = msg.value * tokensPerEth;
+    IERC20(yourToken).safeTransfer(msg.sender, amountOfTokens);
+    emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+  }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
+  function withdraw() public onlyOwner(){
+     (bool success, ) = payable(owner()).call{value: address(this).balance}("");
+      require(success, "Payment failed.");
+  }
 
   // ToDo: create a sellTokens(uint256 _amount) function:
+  function sellTokens(uint256 _amount) public {
+    uint256 eth = _amount / tokensPerEth ;
+    require(yourToken.balanceOf(msg.sender) >= _amount, "You do not have enough tokens");
+    yourToken.safeTransferFrom(msg.sender, address(this), _amount);
+    (bool success, ) = payable(msg.sender).call{value: eth}("");
+    require(success, "Payment failed.");
+    emit SellTokens(msg.sender, _amount, eth);
+   
+  }
 }
